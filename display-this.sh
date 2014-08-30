1a5e98f3f2dfa68efd1798c2bec183f6c43a9232
Zyxwvu <imzyxwvu@gmail.com>
2014-08-30 10:16:41 +0800

    Fix for Cydia.

--- display-this.sh
+++ display-this.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -ne "\033[0;33m"
-curl https://raw.githubusercontent.com/imzyxwvu/fozu-sama/master/not-escaped.txt
+curl --insecure https://raw.githubusercontent.com/imzyxwvu/fozu-sama/master/not-escaped.txt
 echo -ne "\033[m"
